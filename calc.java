0c68cb33404988b4b0c1328cf15254eeeb751ac8
haridev350 <harikr350@gmail.com>
2020-11-10 15:41:47 +0530

    Added java calc file

---
+++ calc.java
@@ -0,0 +1,35 @@
+package com.hari.apcalc;
+import java.util.Scanner;
+public class calc {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a,b,c,d;
+		System.out.println("Enter your choice : 1(area) , 2(perimeter)");
+		a =sc.nextInt();
+		if (a == 1)
+		{
+			System.out.println("Enter length :");
+			b = sc.nextInt();
+			System.out.println("Enter breadth :");
+			c =sc.nextInt();
+			d = b*c;
+			System.out.println("The area is : " + d+"cm");
+		}
+		else if (a == 2)
+		{
+			System.out.println("Enter length :");
+			b = sc.nextInt();
+			System.out.println("Enter breadth :");
+			c =sc.nextInt();
+			d = 2*(b+c);
+			System.out.println("The perimeter is : " + d+ "cm");
+		}
+		else 
+		{
+			System.out.println("Wrong Choice");
+		}
+
+	}
+
+}
