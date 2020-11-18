98efbfe8786e76a4e5573efdb2dec289b5fc6050
haridev350 <harikr350@gmail.com>
2020-11-18 11:16:42 +0530

    Major changes made

--- calc.java
+++ calc.java
@@ -4,30 +4,69 @@ public class calc {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a;
-		double b,c,d;
-		System.out.println("Welcome to Area and perimeter calc v1.1");
-		System.out.println("Enter your choice : 1(area) , 2(perimeter)");
-		a =sc.nextInt();
-		if (a == 1)
+		int v;
+		System.out.println("Welcome to Area and Perimeter Calc v1.2");
+		System.out.println("How do you wish to calculate? 1(normal number) 2(Decimal number)");
+		v = sc.nextInt();
+		if (v == 1)
 		{
-			System.out.println("Enter length :");
-			b = sc.nextDouble();
-			System.out.println("Enter breadth :");
-			c =sc.nextDouble();
-			d = b*c;
-			System.out.println("The area is : " + d+"cm");
+			int a, b,c,d;
+			System.out.println("Enter your choice : 1(area) , 2(perimeter)");
+			a =sc.nextInt();
+			if (a == 1)
+			{
+				System.out.println("Enter length :");
+				b = sc.nextInt();
+				System.out.println("Enter breadth :");
+				c =sc.nextInt();
+				d = b*c;
+				System.out.println("The area is : " + d+"cm");
+			}
+			else if (a == 2)
+			{
+				System.out.println("Enter length :");
+				b = sc.nextInt();
+				System.out.println("Enter breadth :");
+				c =sc.nextInt();
+				d = 2*(b+c);
+				System.out.println("The perimeter is : " + d+ "cm");
+			}
+			else 
+			{
+				System.out.println("Wrong Choice");
+			}
 		}
-		else if (a == 2)
+		else if (v == 2)
 		{
-			System.out.println("Enter length :");
-			b = sc.nextDouble();
-			System.out.println("Enter breadth :");
-			c =sc.nextDouble();
-			d = 2*(b+c);
-			System.out.println("The perimeter is : " + d+ "cm");
+			
+			int a;
+			double b,c,d;
+			System.out.println("Enter your choice : 1(area) , 2(perimeter)");
+			a =sc.nextInt();
+			if (a == 1)
+			{
+				System.out.println("Enter length :");
+				b = sc.nextDouble();
+				System.out.println("Enter breadth :");
+				c =sc.nextDouble();
+				d = b*c;
+				System.out.println("The area is : " + d+"cm");
+			}
+			else if (a == 2)
+			{
+				System.out.println("Enter length :");
+				b = sc.nextDouble();
+				System.out.println("Enter breadth :");
+				c =sc.nextDouble();
+				d = 2*(b+c);
+				System.out.println("The perimeter is : " + d+ "cm");
+			}
+			else 
+			{
+				System.out.println("Wrong Choice");
+			}
 		}
-		else 
+		else
 		{
 			System.out.println("Wrong Choice");
 		}
