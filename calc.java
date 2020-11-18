a417f390dca6b8e5689f13d09349e03212ed37b8
haridev350 <harikr350@gmail.com>
2020-11-18 10:56:41 +0530

    Improved the accuracy of the answers.

--- calc.java
+++ calc.java
@@ -4,24 +4,26 @@ public class calc {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a,b,c,d;
+		int a;
+		double b,c,d;
+		System.out.println("Welcome to Area and perimeter calc v1.1");
 		System.out.println("Enter your choice : 1(area) , 2(perimeter)");
 		a =sc.nextInt();
 		if (a == 1)
 		{
 			System.out.println("Enter length :");
-			b = sc.nextInt();
+			b = sc.nextDouble();
 			System.out.println("Enter breadth :");
-			c =sc.nextInt();
+			c =sc.nextDouble();
 			d = b*c;
 			System.out.println("The area is : " + d+"cm");
 		}
 		else if (a == 2)
 		{
 			System.out.println("Enter length :");
-			b = sc.nextInt();
+			b = sc.nextDouble();
 			System.out.println("Enter breadth :");
-			c =sc.nextInt();
+			c =sc.nextDouble();
 			d = 2*(b+c);
 			System.out.println("The perimeter is : " + d+ "cm");
 		}
